85be0c35f15026c8a3a724c0c9abb74210297b3f
Wybe <wybe@ruurdwestra.nl>
2020-08-08 16:36:32 +0200

    + Setup basic logging.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
+import logging
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 
 
 def main():
+    logging.basicConfig(format="%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        level=logging.DEBUG)
+
     app = QApplication(sys.argv)
 
     window = QWidget()
     window.setWindowTitle("Asset explorer")
+
+    layout = QVBoxLayout()
+
+    test_button = QPushButton(text="Do the thing!")
+    test_button.clicked.connect(button_clicked)
+    layout.addWidget(test_button)
+
+    window.setLayout(layout)
     window.show()
-    
+
     app.exec()
+
+
+def button_clicked():
+    logging.info("Did a thing!")
 
 
 if __name__ == '__main__':
