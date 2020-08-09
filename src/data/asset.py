ec3c15595d85c9f6e73286f73ddec724c7bb7359
Wybe <wybe@ruurdwestra.nl>
2020-08-09 20:14:34 +0200

    + Asset thumbnail now always fits inside a given square.

--- src/data/asset.py
+++ src/data/asset.py
@@ -10,7 +10,8 @@ class Asset:
         self.image = None
         # Thumbnail get's generated on request.
         self.thumbnail = None
-        self.thumbnail_height = 0
+        # Size of the square the current thumbnail fits into.
+        self.thumbnail_size = 0
 
     def get_name(self) -> str:
         return self.file_info.fileName()
@@ -24,20 +25,25 @@ class Asset:
             self.image = QPixmap(self.file_info.absoluteFilePath())
             return self.image
 
-    def load_thumbnail_cached(self, height: int) -> QPixmap:
+    def load_thumbnail_cached(self, size: int) -> QPixmap:
         # TODO: do we want to save thumbnails to disc? Say, to: QStandardPaths::CacheLocation/thumbnails.
         #   We could hash the asset path and use that as the filename.
 
         # TODO: also, make the thumbnail fit in the width.
         #       so the thumbnail fits in a square.
         #       to that end, don't pass "height" but "size"
-        if self.thumbnail and height == self.thumbnail_height:
+        if self.thumbnail and size == self.thumbnail_size:
             # We already have a thumbnail, and it is the right size.
             return self.thumbnail
 
         image = self.load_image_cached()
-        # Scale with preserved aspect ratio, and use bilinear filtering.
-        self.thumbnail = image.scaledToHeight(height, Qt.SmoothTransformation)
-        self.thumbnail_height = height
+        # Scale with preserved aspect ratio, so the image fits in a square with sides of "size".
+        # Uses bilinear filtering.
+        if image.width() < image.height():
+            self.thumbnail = image.scaledToHeight(size, Qt.SmoothTransformation)
+        else:
+            self.thumbnail = image.scaledToWidth(size, Qt.SmoothTransformation)
+
+        self.thumbnail_size = size
 
         return self.thumbnail
