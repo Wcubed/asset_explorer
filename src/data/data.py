eae341c8ca70391f02a396a1a169db9827f952bd
Wybe <wybe@ruurdwestra.nl>
2020-08-09 11:23:22 +0200

    + Asset pack adding now checks for duplicates.

--- src/data/data.py
+++ src/data/data.py
@@ -1,3 +1,5 @@
+import logging
+
 import PyQt5.QtCore as core
 
 from . import asset_pack
@@ -17,12 +19,23 @@ class Data(core.QObject):
 
     def add_asset_pack(self, pack_path: core.QDir):
         # TODO(WWE): Check if the directory is not a subdirectory of one we already have.
-        #   or a duplicate directory.
         #   or a parent directory.
         #   return an exception when that happens.
+
+        # Make sure this isn't a duplicate.
+        for pack in self._asset_packs:
+            if pack.path == pack_path:
+                # It's a duplicate.
+                logging.info("The folder \"{}\" is already loaded as an asset pack.".format(pack_path.absolutePath()))
+                # We don't want to add this one.
+                return
+
         new_pack = asset_pack.AssetPack(pack_path)
 
         self._asset_packs.append(new_pack)
+
+        logging.info("Added asset pack \"{}\" from: \"{}\"".format(new_pack.name, new_pack.path.absolutePath()))
+
         self.pack_added.emit(new_pack.name, new_pack.path)
 
     def add_asset_packs(self, pack_paths: [core.QDir]):
